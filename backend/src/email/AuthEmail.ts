94aadc58d1d7e477601e8e9d667c6587e854b462
luy06D <cusiluis04@gmail.com>
2025-12-29 00:51:28 -0500

    Configuracion email - confirmar cuenta (sendConfirmationEmail)

--- backend/src/email/AuthEmail.ts
+++ backend/src/email/AuthEmail.ts
@@ -1,3 +1,4 @@
+import { transport } from "../config/nodemailer"
 
 type EmailType =  {
     name: string
@@ -8,7 +9,20 @@ type EmailType =  {
 
 export class AuthEmail {
     static sendConfirmationEmail = async (user: EmailType) => {
-        console.log(user);
+        const email = await transport.sendMail({
+            from : 'CashTrackr <admin@cashtrackr.com>',
+            to: user.email,
+            subject: 'CashTrackr - Confirma tu cuenta',
+            html: `<p>Hola_ ${user.name}, has creado tu cuenta de Cashtrackr, ya casi esta lista</p>
+                <p>Visita el siguiente enlace:</p>
+                <a href="#" >Confirma tu cuenta</a>
+                <p>e ingresa el codigo:<b>${user.token}</b></p>
+            
+            `
+        }) 
+
+        console.log(email);
+        
         
     }
 }
