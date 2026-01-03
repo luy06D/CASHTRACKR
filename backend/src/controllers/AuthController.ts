2b66e957a396e79f4f0159670df585d9770aa36c
Luis David <cusiluis04@gmail.com>
2026-01-02 22:57:55 -0500

    add limiter / login / auth / jwt

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,8 +1,9 @@
 import { Response, Request, json } from "express";
 import User from "../models/User"
-import { hashPassword } from "../utils/auth";
+import { comparePassword, hashPassword } from "../utils/auth";
 import { generateToken } from "../utils/token"
 import { AuthEmail } from "../email/AuthEmail";
+import { generateJWT } from "../utils/jwt";
 
 // CREAMOS LOS CONTROLADORES PARA CADA RUTA _ API REST
 export class AuthController {
@@ -34,6 +35,53 @@ export class AuthController {
             res.status(500).json({ error: 'Hubo un error' })
         }
 
+
+    }
+
+    static confirmAccount = async (req: Request, res: Response) => {
+        const { token } = req.body
+
+        const user = await User.findOne({ where: { token } })
+
+        if (!user) {
+            const error = new Error('Token no válido')
+            return res.status(401).json({ error: error.message })
+        }
+
+        user.confirmed = true
+        user.token = null
+        await user.save()
+
+        res.json('Cuenta de usuario confirmada correctamente.')
+
+
+    }
+
+    static login = async (req: Request, res: Response) => {
+
+        const { email, password } = req.body
+
+        const user = await User.findOne({ where: { email } })
+
+        if (!user) {
+            const error = new Error('El email no encontrado')
+            return res.status(402).json({ error: error.message })
+        }
+
+        if (!user.confirmed) {
+            const error = new Error('La cuenta no esta confirmada')
+            return res.status(403).json({ error: error.message })
+        }
+
+        const isPasswordCorrect = await comparePassword(password, user.password)
+
+        if (!isPasswordCorrect) {
+            const error = new Error('La contraseña ingresada es incorrecta')
+            return res.status(403).json({ error: error.message })
+        }
+
+        const token =  generateJWT(user.id)
+        res.json(token)
 
     }
 }
--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -2,14 +2,34 @@ import { Router } from "express";
 import { AuthController } from "../controllers/AuthController";
 import { validateUserErrors } from "../middleware/Authenticate";
 import { handleInputErrors } from "../middleware/validation";
+import { body } from "express-validator";
+import { limiter } from "../config/limiter";
 
 
 const router = Router()
+
+router.use(limiter)
 
 router.post('/create-account',
     validateUserErrors,
     handleInputErrors,
     AuthController.createAccount)
 
+router.post('/confirm-account',
+    body('token')
+        .notEmpty()
+        .isLength({min: 6, max:6})
+        .withMessage('Token no válida'),
+    handleInputErrors,
+    AuthController.confirmAccount)
+
+router.post('/login',
+    body('email')
+        .isEmail().withMessage("Email no válido"),
+    body('password')
+        .notEmpty().withMessage("El password es obligatorio"),
+    handleInputErrors,
+    AuthController.login
+)
 
 export default router
