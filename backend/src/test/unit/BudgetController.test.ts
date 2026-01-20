41456831ef9a5070853e2df897e261deb0c83083
luy06D <cusiluis04@gmail.com>
2026-01-20 00:09:48 -0500

    Testing unitario / getAll complete

--- backend/src/test/unit/BudgetController.test.ts
+++ backend/src/test/unit/BudgetController.test.ts
@@ -1,4 +1,4 @@
-import {createRequest, createResponse} from 'node-mocks-http'
+import { createRequest, createResponse } from 'node-mocks-http'
 import { budgets } from "../mocks/budgets"
 import { BudgetController } from '../../controllers/BudgetController'
 import Budget from '../../models/Budget'
@@ -8,26 +8,97 @@ jest.mock('../../models/Budget', () => ({
     findAll: jest.fn()
 }))
 
-describe('BudgetController.getAll', () =>{
+
+describe('BudgetController.getAll', () => {
+
+    beforeEach(() => {
+        (Budget.findAll as jest.Mock).mockReset();
+        (Budget.findAll as jest.Mock).mockImplementation((options) => {
+            const updateBudgets = budgets.filter(budget => budget.userId === options.where.userId);
+            return Promise.resolve(updateBudgets)
+        })
+
+    })
+
     it('should retrieve 2 budgets for user with ID 1', async () => {
 
         const req = createRequest({
             method: 'GET',
             url: '/api/budgets',
-            user: {id : 2}
-        
+            user: { id: 1 }
+
         })
 
         const res = createResponse();
-
-        const updateBudgets = budgets.filter(budget => budget.userId === req.user.id);
-        (Budget.findAll as jest.Mock).mockResolvedValue(updateBudgets)
         await BudgetController.getAll(req, res)
 
         const data = res._getJSONData()
         expect(data).toHaveLength(2)
         expect(res.statusCode).toBe(200)
         expect(res.status).not.toBe(404)
-        
+
     })
+
+
+    it('should retrieve 1 budgets for user with ID 2', async () => {
+
+        const req = createRequest({
+            method: 'GET',
+            url: '/api/budgets',
+            user: { id: 2 }
+
+        })
+
+        const res = createResponse();
+        await BudgetController.getAll(req, res)
+
+        const data = res._getJSONData()
+        expect(data).toHaveLength(1)
+        expect(res.statusCode).toBe(200)
+        expect(res.status).not.toBe(404)
+
+    })
+
+
+    it('should retrieve 0 budgets for user with ID 10', async () => {
+
+        const req = createRequest({
+            method: 'GET',
+            url: '/api/budgets',
+            user: { id: 10 }
+
+        })
+
+        const res = createResponse();
+        await BudgetController.getAll(req, res)
+
+        const data = res._getJSONData()
+        expect(data).toHaveLength(0)
+        expect(res.statusCode).toBe(200)
+        expect(res.status).not.toBe(404)
+
+    })
+
+
+    
+    it('should handle errors when feching budgets', async () => {
+
+        const req = createRequest({
+            method: 'GET',
+            url: '/api/budgets',
+            user: { id: 100 }
+
+        })
+
+        const res = createResponse();
+        (Budget.findAll as jest.Mock).mockRejectedValue(new Error)
+        await BudgetController.getAll(req, res)
+
+        expect(res.statusCode).toBe(500)
+        expect(res._getJSONData()).toStrictEqual({error: 'Hubo un error'})
+
+
+    })
+
+
 })
