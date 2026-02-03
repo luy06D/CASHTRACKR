ac5170aec989a89693279f810112d782039d0f80
luy06D <cusiluis04@gmail.com>
2026-02-03 00:41:35 -0500

    expenseController testing / getById / updateById / deleteById

--- backend/src/test/unit/controllers/ExpenseController.test.ts
+++ backend/src/test/unit/controllers/ExpenseController.test.ts
@@ -1,12 +1,15 @@
 import { createRequest, createResponse } from "node-mocks-http";
 import { ExpensesController } from "../../../controllers/ExpenseController";
 import Expense from "../../../models/Expense";
+import { budgets } from "../../mocks/budgets";
+import { expenses } from "../../mocks/expenses";
 
 
 
-jest.mock('../../../models/Expense', () => ({ 
-    create: jest.fn()
-  
+jest.mock('../../../models/Expense', () => ({
+    create: jest.fn(),
+    update: jest.fn()
+
 }))
 
 describe('ExpenseController - create', () => {
@@ -21,8 +24,8 @@ describe('ExpenseController - create', () => {
         const req = createRequest({
             method: 'POST',
             url: 'api/budgets/:budgetId/expenses',
-            body: {name: 'prueba gasto', amount: 500 },
-            budget: {id: 1 }
+            body: { name: 'prueba gasto', amount: 500 },
+            budget: { id: 1 }
         })
 
         const res = createResponse();
@@ -35,13 +38,13 @@ describe('ExpenseController - create', () => {
         expect(expenseMock.save).toHaveBeenCalled()
         expect(expenseMock.save).toHaveBeenCalledTimes(1)
         expect(Expense.create).toHaveBeenLastCalledWith(req.body)
-        
-        
+
+
 
     })
 
 
-     it('should handle expense creation error', async () => {
+    it('should handle expense creation error', async () => {
 
         const expenseMock = {
             save: jest.fn().mockResolvedValue(false)
@@ -52,8 +55,8 @@ describe('ExpenseController - create', () => {
         const req = createRequest({
             method: 'POST',
             url: 'api/budgets/:budgetId/expenses',
-            body: {name: 'prueba gasto', amount: 500 },
-            budget: {id: 1 }
+            body: { name: 'prueba gasto', amount: 500 },
+            budget: { id: 1 }
         })
 
         const res = createResponse();
@@ -64,7 +67,89 @@ describe('ExpenseController - create', () => {
         expect(res.statusCode).toBe(500)
         expect(data).toEqual({ error: 'Hubo un error' })
         expect(expenseMock.save).not.toHaveBeenCalled()
-        
+
+
+    })
+})
+
+
+describe('ExpenseController - getById ', () => {
+
+    it('should return expense with ID 1 ', async () => {
+
+
+        const req = createRequest({
+            method: 'GET',
+            url: 'api/budgets/:budgetId/expenses/:expenseId',
+            expense: expenses[0]
+        })
+
+        const res = createResponse();
+        await ExpensesController.getById(req, res)
+
+        const data = res._getJSONData()
+        expect(res.statusCode).toBe(200)
+        expect(data).toEqual(expenses[0])
+
+
+    })
+})
+
+
+describe('ExpenseController - updateById ', () => {
+
+    it('should handle expense Update', async () => {
+
+        const expenseMock = {
+            ...expenses[0],
+            update: jest.fn()
+        };
+
+        const req = createRequest({
+            method: 'PUT',
+            url: 'api/budgets/:budgetId/expenses/:expenseId',
+            expense: expenseMock,
+            body: { name: 'Prueba', amount: 200 },
+        })
+
+        const res = createResponse();
+        await ExpensesController.updateById(req, res)
+
+        const data = res._getJSONData()
+
+        expect(res.statusCode).toBe(200)
+        expect(data).toEqual('Gasto actualizado correctamente')
+        expect(expenseMock.update).toHaveBeenCalled()
+        expect(expenseMock.update).toHaveBeenCalledWith(req.body)
+        expect(expenseMock.update).toHaveBeenCalledTimes(1)
+
+    })
+})
+
+describe('ExpenseController - deleteById ', () => {
+
+    it('should handle expense Delete', async () => {
+
+        const expenseMock = {
+            ...expenses[0],
+            destroy: jest.fn()
+        };
+
+        const req = createRequest({
+            method: 'DELETE',
+            url: 'api/budgets/:budgetId/expenses/:expenseId',
+            expense: expenseMock,
+        })
+
+        const res = createResponse();
+        await ExpensesController.deleteById(req, res)
+
+        const data = res._getJSONData()
+
+        expect(res.statusCode).toBe(200)
+        expect(data).toEqual('Gasto eliminado correctamente')
+        expect(expenseMock.destroy).toHaveBeenCalled()
+        expect(expenseMock.destroy).toHaveBeenCalledTimes(1)
 
     })
 })
