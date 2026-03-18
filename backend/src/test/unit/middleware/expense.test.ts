80a90333507293fb8f63911cb382bbd44825ebd1
LuisD <cusiluis04@gmail.com>
2026-03-17 23:45:02 -0500

    Test para usuario no autorizado

--- backend/src/test/unit/middleware/expense.test.ts
+++ backend/src/test/unit/middleware/expense.test.ts
@@ -2,6 +2,8 @@ import { createRequest, createResponse } from "node-mocks-http";
 import { validateExpenseExist } from "../../../middleware/expense";
 import Expense from "../../../models/Expense";
 import { expenses } from "../../mocks/expenses";
+import { budgets } from "../../mocks/budgets";
+import { hasAccess } from "../../../middleware/budget";
 
 
 jest.mock('../../../models/Expense', () => ({
@@ -51,6 +53,54 @@ describe('Expenses Middleware - validateExpenseExist', () => {
         expect(next).toHaveBeenCalled()
         expect(next).toHaveBeenCalledTimes(1)
         expect(req.expense).toEqual(expenses[0])
+
+    })
+
+    // Test cuando un gasto si existe..
+     it('should handle internal server error', async () => {
+
+        (Expense.findByPk as jest.Mock).mockRejectedValue(new Error)
+        const req = createRequest({
+            params: {expenseId : 1}
+
+        })
+        
+        const res = createResponse();
+        const next = jest.fn();
+        await validateExpenseExist(req, res, next)
+
+        const data = res._getJSONData()
+
+        expect(res.statusCode).toBe(500)
+        expect(next).not.toHaveBeenCalled()
+        expect(data).toEqual({error: 'Hubo un error' })
+
+    })
+
+        // Si un usuario sin autorizacion intenta realizar un gasto..
+     it('should prevent unauthorized users from adding expenses', async () => {
+
+        const req = createRequest({
+            method: 'POST',
+            url: '/api/budgets/:budgetId/expenses',
+            budget: budgets[0],
+            user: {id: 20},
+            body: {name: 'Expenses Test', amount: 3000}
+
+        })
+        
+        const res = createResponse();
+        const next = jest.fn();
+
+        hasAccess(req, res, next)
+
+        const data = res._getJSONData()
+        expect(res.statusCode).toBe(401)
+        expect(data).toEqual({error: 'Acción no valida' })
+
+
+
+       
 
     })
 
