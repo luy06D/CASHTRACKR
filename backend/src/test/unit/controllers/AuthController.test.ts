706f131eb3ce590af12138280075cbbcfb4874ec
Luis David <cusiluis04@gmail.com>
2026-03-31 23:21:04 -0500

    Cambios por resolver...

--- backend/src/test/unit/controllers/AuthController.test.ts
+++ backend/src/test/unit/controllers/AuthController.test.ts
@@ -3,6 +3,8 @@ import User from '../../../models/User'
 import { AuthController } from '../../../controllers/AuthController'
 import { hashPassword } from '../../../utils/auth';
 import { generateToken } from '../../../utils/token';
+import { AuthEmail } from '../../../email/AuthEmail';
+import { token } from 'morgan';
 
 
 jest.mock('../../../models/User');
@@ -10,6 +12,10 @@ jest.mock('../../../utils/auth');
 jest.mock('../../../utils/token');
 
 describe('AuthController.createAccount', () => {
+
+    beforeEach(() => {
+        jest.resetAllMocks();
+    })
 
     it('should return a 409 status and error message if the email is already registered', async () => {
         (User.findOne as jest.Mock).mockResolvedValue(true)
@@ -39,13 +45,14 @@ describe('AuthController.createAccount', () => {
     })
 
     it('should register a new user and return a success message ', async () => {
-     
+
         const req = createRequest({
             method: 'POST',
             url: '/api/auth/create-account',
             body: {
                 email: 'cusi@gmail.com',
-                password: '12345678'
+                password: '12345678',
+                name: 'test Name'
             }
 
         })
@@ -54,10 +61,25 @@ describe('AuthController.createAccount', () => {
         const userMock = { ...req.body, save: jest.fn()};
         
         (User.create as jest.Mock).mockResolvedValue(userMock);
-        (hashPassword as jest.Mock).mockResolvedValue('Simulacion');
-        (generateToken as jest.Mock).mockResolvedValue('12345');
+        (hashPassword as jest.Mock).mockResolvedValue('12345678');
+        (generateToken as jest.Mock).mockResolvedValue('123456');
 
+        jest.spyOn(AuthEmail, "sendConfirmationEmail").mockImplementation(() => Promise.resolve()) ;  
         await AuthController.createAccount(req, res);
+
+        // expect(User.create).toHaveBeenCalledWith(req.body); 
+        // expect(User.create).toHaveBeenCalledTimes(1);
+        // expect(userMock.save).toHaveBeenCalled();
+        expect(userMock.password).toBe('12345678')
+        expect(userMock.token).toBe('123456')
+        expect(AuthEmail.sendConfirmationEmail).toHaveBeenCalledWith({
+            name: req.body.name,
+            email: req.body.email,
+            token: '123456'
+        })
+
+        expect(res.statusCode).toBe(201);   
+        
         
     })
     
