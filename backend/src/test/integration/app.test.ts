f4e645b78d044f159e132dce9476fd851b1f6b21
Luis David <cusiluis04@gmail.com>
2026-04-04 00:13:41 -0500

    Integration Test  / Create Account

--- backend/src/test/integration/app.test.ts
+++ backend/src/test/integration/app.test.ts
@@ -1,5 +1,7 @@
 import request from 'supertest'
 import server from '../../server'
+import { AuthController } from '../../controllers/AuthController'
+
 
 
 describe('Athentication -  Create Account', () => {
@@ -9,10 +11,62 @@ describe('Athentication -  Create Account', () => {
         const response = await request(server)
         .post('/api/auth/create-account')
         .send({})
+        const createAccountMock = jest.spyOn(AuthController, 'createAccount')
 
         expect(response.statusCode).toBe(400)
         expect(response.body).toHaveProperty('errors')
         expect(response.body.errors).toHaveLength(3)
+
+        expect(response.statusCode).not.toBe(201)
+        expect(response.body.errors).not.toHaveLength(5)
+        expect(createAccountMock).not.toHaveBeenCalled()
         
     })
-})
+
+     it('should return 400 when the email is invalid', async() => {
+
+        const response = await request(server)
+        .post('/api/auth/create-account')
+        .send({
+            "name": "LuisD",
+            "password": "12345678",
+            "email": "email_not_valide"
+        })
+        const createAccountMock = jest.spyOn(AuthController, 'createAccount')
+
+        expect(response.statusCode).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+
+        expect(response.statusCode).not.toBe(201)
+        expect(response.body.errors).not.toHaveLength(2)
+        expect(createAccountMock).not.toHaveBeenCalled()
+        
+    })
+
+    it('should return 400 status code when the password is less than 8 characters ', async() => {
+
+        const response = await request(server)
+        .post('/api/auth/create-account')
+        .send({
+            "name": "LuisD",
+            "password": "short",
+            "email": "cusi@gmail.com"
+        })
+        const createAccountMock = jest.spyOn(AuthController, 'createAccount')
+
+        expect(response.statusCode).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+
+        expect(response.body.errors[0].msg).toBe("El password debe tener como minimo 8 caracteres")
+
+        expect(response.statusCode).not.toBe(201)
+        expect(response.body.errors).not.toHaveLength(2)
+        expect(createAccountMock).not.toHaveBeenCalled()
+        
+    })
+
+
+
+}) 
