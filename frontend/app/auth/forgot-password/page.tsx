50b6316358ecfeefbe1fc8d4f07a0c6c7d15b197
LuisD <cusiluis04@gmail.com>
2026-06-19 17:06:04 -0500

    feat: rendering & routing / auth

--- frontend/app/auth/forgot-password/page.tsx
+++ frontend/app/auth/forgot-password/page.tsx
@@ -1,20 +1,21 @@
 import type { Metadata } from "next"
-import RegisterForm from "@/components/Auth/RegisterForm"
+import ForgotForm from "@/components/Auth/ForgotPasswordForm"
 import Link from "next/link"
 
+
 export const metadata: Metadata = {
-    title: "CashTrackr - Crear Cuenta",
-    description: "CashTrackr - Crear Cuenta"
+    title: "CashTrackr - Iniciar Sesión",
+    description: "CashTrackr - Iniciar Sesión"
 }
 
 
 export default function RegisterPage() {
     return (
         <>
-            <h1 className="text-6xl font-black text-purple-950">Crear una cuenta</h1>
-            <p className="text-3xl font-bold">y controla tus <span className="text-amber-500">finanzas</span></p>
+            <h1 className="text-6xl font-black text-purple-950">¿Olvidastes tu contraseña?</h1>
+            <p className="text-3xl font-bold">aqui puedes <span className="text-amber-500">reestablecerla</span></p>
 
-            <RegisterForm />
+            <ForgotForm />
 
             <nav className="mt-10 flex flex-col space-y-4">
                 <Link
@@ -27,10 +28,10 @@ export default function RegisterPage() {
 
             <nav className="mt-10 flex flex-col space-y-4">
                 <Link
-                    href='/auth/forgot-password'
+                    href='/auth/register'
                     className="text-gray-500 text-center"
                 >
-                    ¿Olvidaste tu contraseña? Reestablecer
+                    ¿Aun no tienes una cuenta ? Registrate
                 </Link>
             </nav>
         </>
--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -1,30 +1,30 @@
 import type { Metadata } from "next"
-import RegisterForm from "@/components/Auth/RegisterForm"
+import LoginForm from "@/components/Auth/LoginForm"
 import Link from "next/link"
 
 export const metadata: Metadata = {
-    title: "CashTrackr - Crear Cuenta",
-    description: "CashTrackr - Crear Cuenta"
+    title: "CashTrackr - Iniciar Sesión",
+    description: "CashTrackr - Iniciar Sesión"
 }
 
 
 export default function RegisterPage() {
     return (
         <>
-            <h1 className="text-6xl font-black text-purple-950">Crear una cuenta</h1>
+            <h1 className="text-6xl font-black text-purple-950">Iniciar Sesión</h1>
             <p className="text-3xl font-bold">y controla tus <span className="text-amber-500">finanzas</span></p>
 
-            <RegisterForm />
+            <LoginForm />
+
 
             <nav className="mt-10 flex flex-col space-y-4">
                 <Link
-                    href='/auth/login'
+                    href='/auth/register'
                     className="text-gray-500 text-center"
                 >
-                    ¿Ya tienes cuenta? Iniciar Sesión
+                    ¿Aun no tienes una cuenta ? Registrate
                 </Link>
             </nav>
-
             <nav className="mt-10 flex flex-col space-y-4">
                 <Link
                     href='/auth/forgot-password'
